2576445774f85312655669cd051bef971d2970ef
matthiaskaw <Matthias.Kawalek@t-online.de>
2023-07-30 14:28:01 +0200

    Designed MainWindow and ItemDialog

---
+++ financemanager/ItemDialog.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace financemanager
+{
+    /// <summary>
+    /// Interaktionslogik für ItemDialog.xaml
+    /// </summary>
+    public partial class ItemDialog : Window
+    {
+        public ItemDialog()
+        {
+            InitializeComponent();
+        }
+        public void addContent(string contentname) { 
+           
+        }
+        
+        private void Add_Item_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cancel_Item_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    } 
+}
